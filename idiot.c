20f2dde63ab2cb2acd96641e0deefdff9c12800b
Lee Geonhyeong <foolshift@naver.com>
2017-12-11 16:35:37 +0900

    그림 그리는 함수 미완성

--- idiot.c
+++ idiot.c
@@ -1,22 +1,44 @@
 #include "Altino.h"
+#include <math.h>
+#include <gl/glut.h>
+#include <windows.h>
 
 #define Max 80
 #define Min 20
 #define Crush 150
-#define Speed 350
+#define Speed 400
+#define angle 5
+
+double point[5000][2] = { 0 };
+int i = 0, a = 90, cnt = 1;
 
 int bpm = 115; int oct;
-void s(char sy[3],int len);
+void s(char sy[3], int len);
 
 int stop(int n)
 {
-    if(n<=150)
-    {
-        return 1;
-    }
-    else return 0;
+	if (n <= 150)
+	{
+		return 1;
+	}
+	else return 0;
 }
 
+void display()
+{
+	int j;
+
+	glPointSize(5.0f);
+	glBegin(GL_LINE_STRIP);
+	glVertex2f(0, 0);
+	for (j = 0; j < i; j++)
+	{
+		glVertex2f(point[j][0]/20, point[j][1]/20);
+	}
+
+	glEnd();
+	glFlush();
+}
 void backsound()
 {
 	s("E .", 8); s("D#.", 8); s("E .", 8); s("D#.", 8); s("E .", 8); s("B", 8); s("D .", 8); s("C .", 8); s("A", 4); s("", 8);
@@ -25,148 +47,136 @@ void backsound()
 	s("C", 8); s("E", 8); s("A", 8); s("B", 4); s("", 8); s("E", 8); s("C .", 8); s("B", 8); s("A", 4); s("", 8);
 	s("B", 8); s("C .", 8); s("D .", 8); s("E .", 4); s("", 8); s("G", 8); s("F .", 8); s("E .", 8); s("D .", 4); s("", 8);
 	s("F", 8); s("E .", 8); s("D .", 8); s("C .", 4); s("", 8); s("E", 8); s("D .", 8); s("C .", 8); s("B", 4); s("", 8);*/
-    Sound(0);
+	Sound(0);
 }
 
 void s(char sy[3], int len)
 {
 	int mel = 0;
 	switch (sy[0])
-		{case 'C': {mel = 1;} break;
-		case 'D': {mel = 3;} break;
-		case 'E': {mel = 5;} break;
-		case 'F': {mel = 6;} break;
-		case 'G': {mel = 8;} break;
-		case 'A': {mel = 10;} break;
-		case 'B': {mel = 12;} break;
-		}
+	{
+	case 'C': {mel = 1; } break;
+	case 'D': {mel = 3; } break;
+	case 'E': {mel = 5; } break;
+	case 'F': {mel = 6; } break;
+	case 'G': {mel = 8; } break;
+	case 'A': {mel = 10; } break;
+	case 'B': {mel = 12; } break;
+	}
 
 	switch (sy[1])
-		{case '#': {mel += 1;} break;
-		case 'b': {mel -= 1;} break;
-		}
+	{
+	case '#': {mel += 1; } break;
+	case 'b': {mel -= 1; } break;
+	}
 
-	switch(sy[2])
-		{case '.': {oct = 5;} break;
-		default: {oct = 4;} break;
-		}
+	switch (sy[2])
+	{
+	case '.': {oct = 5; } break;
+	default: {oct = 4; } break;
+	}
 	mel += (oct - 1) * 12;
 
 	Sound(mel);
-	delay(120000/len/bpm);
+	delay(120000 / len / bpm);
 }
 
 void print_IR_value(int ir)
 {
-    printf("%d ", ir);
+	printf("%d ", ir);
 }
 
 void retreat() {
-    Steering(1);
-	Go(-Speed, -Speed);
-	delay(1000);
+	Steering(1);
+	a += angle;
+	Go(-Speed - 100, -Speed - 100);
+	delay(500);
+	point[i][0] = point[i - 1][0] - cos(a*3.14 / 180)/100;
+	point[i][1] = point[i - 1][0] - sin(a*3.14 / 180)/100;
+	i++;
 	//backsound();
+	Steering(2);
 	Go(Speed, Speed);
 }
 
 void wonidea()
 {
-    SensorData sens;
-    int ir0, ir1, ir2, ir3, ir4, ir5, jodo;
+	SensorData sens;
+	int ir0, ir1, ir2, ir3, ir4, ir5, jodo;
 
-     while(1)
-    {
-        sens = Sensor(10);
-        ir0 = sens.IRSensor[0];
-        ir1 = sens.IRSensor[1];
-        ir2 = sens.IRSensor[2];
-        ir3 = sens.IRSensor[3];
-        ir4 = sens.IRSensor[4];
-        ir5 = sens.IRSensor[5];
-        jodo = sens.CDSSensor;
-        print_IR_value(ir0);
-        print_IR_value(ir1);
-        print_IR_value(ir2);
-        print_IR_value(ir3);
-        print_IR_value(ir4);
-        print_IR_value(ir5);
-        printf("\n");
+	while (1)
+	{
+		sens = Sensor(10);
+		ir0 = sens.IRSensor[0];
+		ir1 = sens.IRSensor[1];
+		ir2 = sens.IRSensor[2];
+		ir3 = sens.IRSensor[3];
+		ir4 = sens.IRSensor[4];
+		ir5 = sens.IRSensor[5];
+		jodo = sens.CDSSensor;
 
-        if(stop(jodo)==1)
-        {
-            break;
-        }
+		print_IR_value(ir0);
+		print_IR_value(ir1);
+		print_IR_value(ir2);
+		print_IR_value(ir3);
+		print_IR_value(ir4);
+		print_IR_value(ir5);
+		printf("\n");
 
-        if (ir1 < 5)
-        {
-            if (ir0 > 5) Steering(2);
-            else Steering(1);
-        }
-        else if (ir0 > 0 && ir1 >= 5)
-        {
-            Steering(3);
-            if (ir0 > 250 || ir1 > 250) retreat();
-        }
-    }
+		if (stop(jodo) == 1)
+		{
+			break;
+		}
 
-}
+		if (ir1 < 5)
+		{
+			if (ir0 > 5)
+			{
+				Steering(2);
+			}
+			else
+			{
+				Steering(1);
+				a += angle;
+			}
+		}
+		else if (ir0 > 0 && ir1 >= 5)
+		{
+			Steering(3);
+			a -= angle;
+			if (ir0 > 250 || ir1 > 250)
+			{
+				retreat();
+				continue;
+			}
+		}
+		if (i == 0)
+		{
+			point[i][0] = cos(a*3.14 / 180);
+			point[i][1] = sin(a*3.14 / 180);
+		}
+		else
+		{
+			point[i][0] = point[i - 1][0] + cos(a*3.14 / 180);
+			point[i][1] = point[i - 1][1] + sin(a*3.14 / 180);
+		}
+		i++;
+	}
 
-void dongidea()
-{
-    SensorData sens;
-    int ir0, ir1, ir2, ir3, ir4, ir5;
-
-     while(1)
-    {
-        sens = Sensor(10);
-        ir0 = sens.IRSensor[0];
-        ir1 = sens.IRSensor[1];
-        ir2 = sens.IRSensor[2];
-        ir3 = sens.IRSensor[3];
-        ir4 = sens.IRSensor[4];
-        ir5 = sens.IRSensor[5];
-        print_IR_value(ir0);
-        print_IR_value(ir1);
-        print_IR_value(ir2);
-        print_IR_value(ir3);
-        print_IR_value(ir4);
-        print_IR_value(ir5);
-        printf("\n");
-
-        if (ir1 >= Crush)
-        {
-            retreat();
-        }
-        else
-        {
-            if(ir0 >= Crush)
-            {
-                retreat();
-            }
-            else if(ir0 > Min)
-            {
-                if (ir4 > Max) Steering(3);
-                else if (ir4 > Min) Steering(2);
-                else Steering(1);
-            }
-            else
-            {
-                if (ir4 > Max) Steering(2);
-                else if (ir4 > Min) Steering(2);
-                else Steering(1);
-            }
-        }
-
-    }
 }
 
 int main()
 {
-    Open(szPort);
+	Open(szPort);
 
-    Go(Speed,Speed);
-    //dongidea();
-    wonidea();
+	Go(Speed, Speed);
+	wonidea();
+	Go(0, 0);
+	glutInitDisplayMode(GLUT_RGBA);
+	glutInitWindowSize(1000, 500);
+	glutCreateWindow("opengl project");
+	glutDisplayFunc(display);
+	glutMainLoop();
 
-    return 0;
+	return 0;
 }
